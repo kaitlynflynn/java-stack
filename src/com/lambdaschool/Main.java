f431451e75b48ed2eb317e2126a54475289b5ea8
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-07 15:52:18 -0800

    Created new Stack and implemented functionality for methods; output prints successfully

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -1,8 +1,23 @@
 package com.lambdaschool;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	    Stack myStack = new Stack(); // make new stack
+	    myStack.numOfItems(); // # of items on stack
+        myStack.push("'first'"); // Push 1st item
+        myStack.push("'second'"); // Push 2nd item
+        myStack.push("'third'"); // Push 3rd item
+        myStack.numOfItems(); // # of items on stack
+        myStack.print(); // Print stack
+        myStack.pop(); // Pop last item from stack
+        myStack.print(); // Print stack
+        myStack.pop(); // Pop last item from stack
+        myStack.print(); // Print stack
+        myStack.pop(); // Pop last item from stack
+        myStack.print(); // Print stack
+        myStack.numOfItems(); // # of items on stack
     }
 }
